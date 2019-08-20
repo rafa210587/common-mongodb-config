25d9dfb5e78c3fa47d3dc26fed009e3b4711adcc
Rafael Meira Goncalves <rafael.goncalves@trustpay.com.br>
2019-08-20 18:26:58 -0300

    fix in pom

--- src/main/java/comum/mongodb/config/MongoDBConfig.java
+++ src/main/java/comum/mongodb/config/MongoDBConfig.java
@@ -1,16 +1,10 @@
 package comum.mongodb.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.mongo.MongoProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.mongodb.MongoClient;
 
